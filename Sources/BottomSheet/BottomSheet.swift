ac17d563f27f222d1553f94e090946c521dad3bf
mostafa alsayed <mostafa.alsayed0123@gmail.com>
2020-05-30 05:04:03 +0200

    Update presentation style

--- Sources/BottomSheet/BottomSheet.swift
+++ Sources/BottomSheet/BottomSheet.swift
@@ -1,6 +1,14 @@
-class BottomSheet {
+public class BottomSheet {
     
+    
+    /// Create simple bottom sheet
     class func createBottomSheet() {
+        print("Bottom sheet created")
+    }
+    
+    /// Create bottom sheet with animation
+    /// - Parameter animated: <#animated description#>
+    internal class func createBottomSheet(animated: Bool) {
         print("Bottom sheet created")
     }
 }
