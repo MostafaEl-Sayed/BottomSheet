a695d01dc2b5a95cc269d2b012fedb549956edf0
mostafa alsayed <mostafa.alsayed0123@gmail.com>
2020-05-30 18:18:33 +0200

    Remove animated creation feature

--- Sources/BottomSheet/BottomSheet.swift
+++ Sources/BottomSheet/BottomSheet.swift
@@ -1,14 +1,8 @@
 public class BottomSheet {
-    
-    
+
     /// Create simple bottom sheet
-    class func createBottomSheet() {
+    public class func createBottomSheet() {
         print("Bottom sheet created")
     }
-    
-    /// Create bottom sheet with animation
-    /// - Parameter animated: <#animated description#>
-    internal class func createBottomSheet(animated: Bool) {
-        print("Bottom sheet created")
-    }
+
 }
